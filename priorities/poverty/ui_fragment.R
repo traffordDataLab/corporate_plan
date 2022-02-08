165b11c38250fefcc22ad802d612570607036505
James Austin <james.austin@trafford.gov.uk>
2022-02-08 19:04:25 +0000

    Added in commented references to each of the indicators.

--- priorities/poverty/ui_fragment.R
+++ priorities/poverty/ui_fragment.R
@@ -6,33 +6,43 @@ tabPanel(
     h2("Supporting people out of poverty"),
     includeHTML("help.html"),
     fluidRow(
+        # Percentage receiving Universal Credit (UC) and the Claimant Count (CC) ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
             
         #),
+        # Number of people prevented from becoming homeless ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Improve the number of affordable housing completions ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Reduction in % of children in poverty ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Maintain the low level of 16-17 year olds who are NEET and NEET plus unknown ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Improve the number of people being re-housed (from Trafford’s housing waiting list) ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Reduce % of households fuel poverty levels ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Improve overall employment rate (aged 16-64) (%) ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Improve employees paid at/above the real living wage ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #),
+        # Improve school readiness all children and those with a free school meal status ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
         #)
