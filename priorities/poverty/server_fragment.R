dba154cdec42418b3bfc7684a7170c3a0056b427
IGRiosTC <iris.garciarios@trafford.gov.uk>
2022-02-16 12:57:14 +0000

    Adding Employment rate indicator

--- priorities/poverty/server_fragment.R
+++ priorities/poverty/server_fragment.R
@@ -158,22 +158,65 @@
 
 # Load in data
 
+employment_rate <- read_csv("data/poverty/employment_rate.csv") #%>%
+  #mutate(period = as_factor(period)) %>%
+  #filter(!is.na(value))
+
+employment_rate_cipfa_mean <- employment_rate %>%
+  filter(area_code %in% c(cipfa$area_code)) %>%
+  group_by(period) %>%
+  summarise(value = round(mean(value, na.rm=TRUE), 1)) %>%
+  mutate(area_name = "Similar Authorities average"#,
+         #period = as_factor(period)
+         ) %>%
+  filter(!is.na(value))
+
+employment_rate_trend <- bind_rows(employment_rate %>% select(area_name, period,value) %>% filter(area_name %in% c("Trafford", "England")), employment_rate_cipfa_mean) %>%
+  mutate(period = str_sub(period, start = 10))
 
 # Plot
-#output$[INDICATOR NAME]_plot <- renderggiraph({
+output$employment_rate_plot <- renderggiraph({
+  
+  if (input$employment_rate_selection == "Trend") {
+    
+    gg <- ggplot(
+      filter(employment_rate_trend, area_name %in% c("Trafford", "Similar Authorities average", "England")),
+      aes(x = period, y = value, colour = area_name, fill = area_name, group = area_name)) +
+      geom_line(size = 1) +
+      geom_point_interactive(aes(tooltip =
+                                   paste0('<span class="plotTooltipValue">', value, '%</span><br />',
+                                          '<span class="plotTooltipMain">', area_name, '</span><br />',
+                                          '<span class="plotTooltipPeriod">', period, '</span>')),
+                             shape = 21, size = 2.5, colour = "white") +
+      scale_colour_manual(values = c("Trafford" = plot_colour_trafford, "Similar Authorities average" = plot_colour_similar_authorities, "England" = plot_colour_england)) +
+      scale_fill_manual(values = c("Trafford" = plot_colour_trafford, "Similar Authorities average" = plot_colour_similar_authorities, "England" = plot_colour_england)) +
+      scale_y_continuous(limits = c(0, NA)) +
+      labs(
+        title = "Employment rate - aged 16 to 64",
+        subtitle = NULL,
+        caption = "Source: Annual Polulation Survey",
+        x = "12 months ending",
+        y = "Percentage",
+        colour = NULL
+      ) +
+      theme_x()
+  }
+  
+  girafe(ggobj = gg, options = lab_ggiraph_options)
+  
 
-#})
+})
 
 # Render the output in the ui object
-# output$[INDICATOR NAME]_box <- renderUI({
-#   withSpinner(
-#     ggiraphOutput("[INDICATOR NAME]_plot", height = "inherit"),
-#     type = 4,
-#     color = plot_colour_spinner,
-#     size = 1,
-#     proxy.height = "250px"
-#   )
-# })
+output$employment_rate_box <- renderUI({
+  withSpinner(
+    ggiraphOutput("employment_rate_plot", height = "inherit"),
+    type = 4,
+    color = plot_colour_spinner,
+    size = 1,
+    proxy.height = "250px"
+  )
+})
 
 
 # Improve employees paid at/above the real living wage ---------
--- priorities/poverty/ui_fragment.R
+++ priorities/poverty/ui_fragment.R
@@ -35,9 +35,24 @@ tabPanel(
         
         #),
         # Improve overall employment rate (aged 16-64) (%) ---------
-        #div(class = "col-sm-12 col-md-6 col-lg-4",
-        
-        #),
+        div(class = "col-sm-12 col-md-6 col-lg-4",
+            h3("Employment rate"),
+            uiOutput("employment_rate_box", class = "indicatorContainer"),
+            radioGroupButtons(
+              inputId = "employment_rate_selection",
+              choiceNames = c("Trend"),
+              choiceValues = c("Trend"),
+              selected = "Trend",
+              direction = "horizontal",
+              individual = FALSE,
+              status = "plotButtons" # Our custom CSS class, .btn-plotButtons
+            ),
+            HTML('<details class="furtherInfo">
+                    <summary>Further information</summary>'),
+            includeMarkdown("data/poverty/metadata/employment_rate.md"),
+            HTML('</details>')
+        )
+
         # Improve employees paid at/above the real living wage ---------
         #div(class = "col-sm-12 col-md-6 col-lg-4",
         
