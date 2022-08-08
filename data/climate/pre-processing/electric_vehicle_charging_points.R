4dec21c65107c39e3bdb8bc63d27c9e449fc05cb
James Austin <james.austin@trafford.gov.uk>
2022-08-08 13:29:45 +0100

    Updated the vehicle charging points indicator with the latest data and compared with the 2021 CIPFA nearest neighbours.

--- data/climate/pre-processing/electric_vehicle_charging_points.R
+++ data/climate/pre-processing/electric_vehicle_charging_points.R
@@ -2,21 +2,21 @@
 # Created: 2022-01-10
 
 # Source: Department for Transport (DfT) and Office for Zero Emission Vehicles (OZEV)
-#         https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-april-2022
-#         https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1069013/electric-vehicle-charging-device-statistics-april-2022.ods
+#         https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-july-2022
+#         https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1091845/electric-vehicle-charging-device-statistics-july-2022.ods
 
 
 # Load required packages ---------------------------
-library(tidyverse); library(readODS);
+library(tidyverse); library(readODS); library(httr)
 
 # Download the data ---------------------------
 tmp <- tempfile(fileext = ".ods")
-GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1069013/electric-vehicle-charging-device-statistics-april-2022.ods",
+GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1091845/electric-vehicle-charging-device-statistics-july-2022.ods",
     write_disk(tmp))
 
 # Setup objects ---------------------------
 # Trafford, its CIPFA nearest neighbours (2019) and England:
-authorities <- read_csv("../../cipfa2019.csv") %>%
+authorities <- read_csv("../../cipfa2021.csv") %>%
   add_row(area_code = "E08000009", area_name = "Trafford") %>%
   add_row(area_code = "E92000001", area_name = "England")
 
@@ -26,20 +26,21 @@ df_raw <- read_ods(tmp, sheet = 2, col_names = TRUE, col_types = NA, skip = 6)
 # Tidy the data ---------------------------
 df_charging_points_rate <- df_raw %>%
   # Due to merged cells the source spreadsheet, the headings are misaligned/missing. Rename them and select the ones we want all within the select().
-  # NOTE: due to the misalignment of the headings the renamed columns look like a mistake but they are correct!
+  # NOTE: due to the misalignment of the headings it's better to reference the column number
   select(area_code = `LA / Region Code`,
          area_name = `Local Authority / Region Name`,
-         `2022-04` = `Jan-22`,
-         `2022-01` = `Jul-21`,
-         `2021-10` = `Jan-21`,
-         `2021-07` = `Jul-20`,
-         `2021-04` = `Jan-20`,
-         `2021-01` = 14,
-         `2020-10` = 16,
-         `2020-07` = 18,
-         `2020-04` = 20,
-         `2020-01` = 22,
-         `2019-10` = 24
+         `2022-07` = 4,
+         `2022-04` = 6,
+         `2022-01` = 8,
+         `2021-10` = 10,
+         `2021-07` = 12,
+         `2021-04` = 14,
+         `2021-01` = 16,
+         `2020-10` = 18,
+         `2020-07` = 20,
+         `2020-04` = 22,
+         `2020-01` = 24,
+         `2019-10` = 26
   ) %>%
   filter(area_code %in% authorities$area_code) %>%
   mutate(area_name = if_else(area_name == "ENGLAND", "England", area_name)) %>%
