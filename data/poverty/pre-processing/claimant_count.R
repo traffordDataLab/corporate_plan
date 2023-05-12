1a058bb24b5a9b03188e98ac8a079ef6a8241309
IGRiosTC <iris.garciarios@trafford.gov.uk>
2023-05-12 12:07:01 +0100

    Update Claimant count

--- data/poverty/pre-processing/claimant_count.R
+++ data/poverty/pre-processing/claimant_count.R
@@ -15,11 +15,10 @@ df <- read_csv(paste0("https://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.
          area_name = GEOGRAPHY_NAME,
          period = DATE_NAME,
          indicator = MEASURE_NAME, measure, units,
-         value = OBS_VALUE)
+         value = OBS_VALUE) %>%
+  unique()
 
-# Ward level for maps
-
-df_wards <- read_csv("https://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?geography=1656750701...1656750715,1656750717,1656750716,1656750718...1656750721&date=latest&gender=0&age=0&measure=2&measures=20100") %>%
+df_wards <- read_csv("https://www.nomisweb.co.uk/api/v01/dataset/NM_162_1.data.csv?geography=1648362073...1648362087,1648362089,1648362088,1648362090...1648362093&date=latest&gender=0&age=0&measure=1,2&measures=20100") %>%
   mutate(units = "Persons",
          measure = ifelse(MEASURE_NAME == "Claimant count", "Count", "Percentage")) %>%
   select(area_code = GEOGRAPHY_CODE,
