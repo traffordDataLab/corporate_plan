4f9ec1184c38fd5fbfe62fe853ded1e95bb9de60
James Austin <james.austin@trafford.gov.uk>
2023-09-22 19:51:48 +0100

    Updated proportion of adults walking or cycling 5 or more times per week to latest data available 2015/16 - 2021/22 published on 2023-08-30.

--- data/climate/metadata/adults_walking_or_cycling.md
+++ data/climate/metadata/adults_walking_or_cycling.md
@@ -2,7 +2,7 @@
 
 Note: the following links open in a new window.
 
-Source: Dataset CW0301: <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1019282/cw0301.ods" target="_blank">Proportion of adults who do any walking or cycling, for any purpose, by frequency and local authority: England</a> from <a href="https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw" target="_blank">Department for Transport</a>.
+Source: Dataset CW0301: <a href="https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1181185/cw0301-proportion-of-adults-who-do-any-walking-or-cycling-by-frequency-purpose-and-local-authority.ods" target="_blank">Proportion of adults who do any walking or cycling, for any purpose, by frequency and local authority: England</a> from <a href="https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw" target="_blank">Department for Transport</a>.
 
 Further information regarding this dataset can be found in <a href="https://www.gov.uk/government/publications/walking-and-cycling-statistics-notes-and-definitions/walking-and-cycling-statistics-notes-and-definitions" target="_blank">Walking and cycling statistics: notes and definitions</a>
 
--- data/climate/pre-processing/adults_walking_or_cycling.R
+++ data/climate/pre-processing/adults_walking_or_cycling.R
@@ -1,9 +1,9 @@
 # Proportion of adults who do any walking or cycling, for any purpose, five times per week
-# Created: 2022-02-07
+# Created: 2022-02-07  Updated: 2023-09-27  Data: 2023-08-30
 
 # Source: Department for Transport (DfT)
 #         https://www.gov.uk/government/statistical-data-sets/walking-and-cycling-statistics-cw#participation-in-walking-and-cycling
-#         CW0301: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1100941/cw0301.ods
+#         CW0301: https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1181185/cw0301-proportion-of-adults-who-do-any-walking-or-cycling-by-frequency-purpose-and-local-authority.ods
 
 
 # Load required packages ---------------------------
@@ -17,19 +17,27 @@ authorities <- read_csv("../../cipfa2021.csv") %>%
 
 # Download the data ---------------------------
 tmp <- tempfile(fileext = ".ods")
-GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1100941/cw0301.ods",
+GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1181185/cw0301-proportion-of-adults-who-do-any-walking-or-cycling-by-frequency-purpose-and-local-authority.ods",
     write_disk(tmp))
 
-df_raw <- read_ods(tmp, sheet = "CW0301", skip = 7)
+df_raw <- read_ods(tmp, sheet = "CW0301_Any_Purpose", skip = 4)
 
 # Tidy the data ---------------------------
 df_wlk_cyc <- df_raw %>%
-  rename(area_code = `ONS Code`,
-         area_name = `Area name`) %>%
+  rename(area_code = `ONS.Code`,
+         area_name = `Area.name`) %>%
   filter(Frequency == "At least 5 times per week",
          area_code %in% authorities$area_code) %>%
-  select(-Mode, -Purpose, -Frequency) %>% # remove unwanted columns
-  mutate(`2021` = as.character(`2021`)) %>% # annoyingly this column is num when the others are char - need all the same to pivot
+  select(-Class, -Mode, -Purpose, -Frequency) %>% # remove unwanted columns
+  rename(`2016` = `X2016`,
+         `2017` = `X2017`,
+         `2018` = `X2018`,
+         `2019` = `X2019`,
+         `2020` = `X2020`,
+         `2021` = `X2021`,
+         `2022` = `X2022`) %>%  
+    mutate(`2021` = as.character(`2021`),
+           `2022` = as.character(`2022`)) %>% # annoyingly these columns are num when the others are char - need all the same to pivot
   pivot_longer(cols = c(-area_code, -area_name),
                names_to = "period",
                values_to = "value") %>%
