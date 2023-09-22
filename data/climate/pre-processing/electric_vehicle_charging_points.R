6b752dd6d0315b66ee03b5aa425ff4e58bda5256
James Austin <james.austin@trafford.gov.uk>
2023-09-22 14:16:40 +0100

    Updated electric vehicle charging devices to latest data available: July 2023, published on 2023-07-26.

--- data/climate/pre-processing/electric_vehicle_charging_points.R
+++ data/climate/pre-processing/electric_vehicle_charging_points.R
@@ -1,10 +1,10 @@
 # Electric vehicle charging points.
-# Created: 2022-01-10
+# Created: 2022-01-10  Updated: 2023-09-22  Data: 2023-07-26
 
 # Source: Department for Transport (DfT) and Office for Zero Emission Vehicles (OZEV)
 #         https://www.gov.uk/government/collections/electric-vehicle-charging-infrastructure-statistics
-#         https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-april-2023
-#         https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1154215/electric-vehicle-charging-device-statistics-april-2023.ods
+#         https://www.gov.uk/government/statistics/electric-vehicle-charging-device-statistics-july-2023
+#         https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1173344/electric-vehicle-charging-device-statistics-july-2023.ods
 
 
 # Load required packages ---------------------------
@@ -12,7 +12,7 @@ library(tidyverse); library(tidyselect); library(readODS); library(httr)
 
 # Download the data ---------------------------
 tmp <- tempfile(fileext = ".ods")
-GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1154215/electric-vehicle-charging-device-statistics-april-2023.ods",
+GET(url = "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/attachment_data/file/1173344/electric-vehicle-charging-device-statistics-july-2023.ods",
     write_disk(tmp))
 
 # Setup objects ---------------------------
@@ -28,24 +28,24 @@ df_raw <- read_ods(tmp, sheet = "1a", col_names = TRUE, col_types = NA, skip = 2
 df_charging_points_rate <- df_raw %>%
   # Due to merged cells the source spreadsheet, the headings are misaligned/missing. Rename them and select the ones we want all within the select().
   # NOTE: due to the misalignment of the headings it's better to reference the column number
-  select(area_code = `Local Authority / Region Code`,
-         area_name = `Local Authority / Region Name`,
-         vars_select(names(df_raw), contains('population')) # we only want the data which is per 100K population
+  select(area_code = `Local.Authority...Region.Code`,
+         area_name = `Local.Authority...Region.Name`,
+         contains('population') # we only want the data which is per 100K population
   ) %>%
   # We need to rename the first 12 columns of data to the correct date format we need for the chart
   rename(
-    `2023-04` = 3,
-    `2023-01` = 4,
-    `2022-10` = 5,
-    `2022-07` = 6,
-    `2022-04` = 7,
-    `2022-01` = 8,
-    `2021-10` = 9,
-    `2021-07` = 10,
-    `2021-04` = 11,
-    `2021-01` = 12,
-    `2020-10` = 13,
-    `2020-07` = 14
+    `2023-07` = 3,
+    `2023-04` = 4,
+    `2023-01` = 5,
+    `2022-10` = 6,
+    `2022-07` = 7,
+    `2022-04` = 8,
+    `2022-01` = 9,
+    `2021-10` = 10,
+    `2021-07` = 11,
+    `2021-04` = 12,
+    `2021-01` = 13,
+    `2020-10` = 14
   ) %>%
   select(1:14) %>% # Now just select the first 14 columns of data
   filter(area_code %in% authorities$area_code) %>%
