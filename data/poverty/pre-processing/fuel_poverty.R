f11dfa5b6828a2af1804e9b998371e7b3e831746
IGRiosTC <32395357+IGRiosTC@users.noreply.github.com>
2024-05-16 08:51:41 +0100

    Update Fuel poverty to 2022

--- data/poverty/pre-processing/fuel_poverty.R
+++ data/poverty/pre-processing/fuel_poverty.R
@@ -26,7 +26,7 @@ fuel_poverty_LIHC <- read_csv(paste0("https://webservices.esd.org.uk/data.csv?me
 
 fuel_poverty_LILEE <- read_csv(paste0("https://webservices.esd.org.uk/data.csv?metricType=16259&area=",paste(c("E92000001",cipfa$area_code, "E08000009"), collapse = ','),"&period=latest:5&columnGrouping=period&rowGrouping=area&ApplicationKey=",lginform_key)) %>%
   filter(area != "area") %>%
-  pivot_longer("2019":"2021", names_to = 'period', values_to = 'value') %>%
+  pivot_longer("2019":"2022", names_to = 'period', values_to = 'value') %>%
   select(area_name = `area label`, area_code = area, period, value) %>%
   mutate(indicator = "Fuel poverty (low income, low energy efficiency methodology)",
          measure = "Percentage", unit = "Persons", compared_to_England = NA_character_, inequality = NA_character_,
@@ -41,6 +41,7 @@ fuel_poverty_LILEE <- read_csv(paste0("https://webservices.esd.org.uk/data.csv?m
 df <- bind_rows(fuel_poverty_LIHC, fuel_poverty_LILEE) %>%
   mutate(value = round(value, 1)) %>%
   mutate(measure = "Percentage", unit = "Households") %>%
-  select(area_code, area_name, period, indicator, measure, unit, value)
+  select(area_code, area_name, period, indicator, measure, unit, value) %>%
+  arrange(period)
 
 write_csv(df, "../fuel_poverty.csv")
